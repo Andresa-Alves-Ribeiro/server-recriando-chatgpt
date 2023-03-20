78d21a0235073516d1558975e73810d02a33a581
Andresa Alves <andresa_15ga@hotmail.com>
2023-03-20 16:41:12 -0300

    removendo dist

--- dist/routes/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var express = require("express");
-var controller = require("../controllers/controller");
-var routes = express.Router();
-routes.post('/api/prompt', controller.sendText);
-module.exports = routes;
--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var app = require("./app.js");
-var port = process.env.PORT;
-app.listen(port, function () {
-  console.log("Server ".concat(port));
-});
