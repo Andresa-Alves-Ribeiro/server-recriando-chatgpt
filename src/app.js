06374834cf11e01e5f716933dbf1380d1b425540
Andresa Alves <andresa_15ga@hotmail.com>
2023-03-20 16:34:07 -0300

    removendo front

--- src/app.js
+++ src/app.js
@@ -1,62 +1,13 @@
-import { useState } from 'react';
-import './App.css';
-import './styles/reset.css';
-import ChatMessage from './components/ChatMessage/ChatMessage'
-import SideMenu from './components/SideMenu/Sidemenu'
-import { makeRequest } from './api/api'
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes/routes");
 
+require('dotenv').config()
 
-function App() {
-  const [input, setInput] = useState("")
-  const [chatLog, setChatLog] = useState([{
-    user: "gpt",
-    message: "How can I help you today?"
-  }])
+const app = express()
 
-  async function handleSubmit(e) {
-    e.preventDefault();
+app.use(express.json())
+app.use(cors())
+app.use(routes)
 
-    let response = await makeRequest({ prompt: input })
-
-    response = response.data.split('\n')
-      .map(line => <p>{line}</p>);
-
-    setChatLog([...chatLog, {
-      user: 'me',
-      message: `${input}`
-    }, {
-      user: 'gpt',
-      message: response
-    }])
-    setInput("")
-  }
-
-  return (
-    <div className="App">
-      <SideMenu></SideMenu>
-
-      <section className='chatbox'>
-
-        <div className='chat-log'>
-          {chatLog.map((message, index) => (
-            <ChatMessage key={index} message={message} />
-          ))}
-        </div>
-
-        <div className='chat-input-holder'>
-          <form onSubmit={handleSubmit}>
-            <input
-              rows='1'
-              className='chat-input-textarea'
-              value={input}
-              onChange={e => setInput(e.target.value)}
-            >
-            </input>
-          </form>
-        </div>
-      </section>
-    </div>
-  );
-}
-
-export default App;
+module.exports = app;
